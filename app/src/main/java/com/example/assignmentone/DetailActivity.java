1a1ea726d74a342229f938db64886de6f727d26e
wbhaoshuai <1402078577@qq.com>
2025-10-25 19:58:14 +0800

    Added the ability to change content based on clicking on different imageviews.
    Based on the received location index, locate the corresponding content in the array file and fill it in the corresponding position.

--- app/src/main/java/com/example/assignmentone/DetailActivity.java
+++ app/src/main/java/com/example/assignmentone/DetailActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,5 +23,27 @@ public class DetailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // 0:Facilities, 1:Events, 2:Clubs, 3:Support
+        int itemIndex = getIntent().getIntExtra("POSITION", 0);
+
+        String[] imageNames = getResources().getStringArray(R.array.string_array_images);
+        String[] titles = getResources().getStringArray(R.array.string_array_titles);
+        String[] contents = getResources().getStringArray(R.array.string_array_content);
+
+        String[] shortTitles = {"Facilities", "Events", "Clubs", "Support"};
+
+        ImageView detailImageView = findViewById(R.id.detailImageView);
+        TextView itemTextView = findViewById(R.id.ItemTextView);
+        TextView titleTextView = findViewById(R.id.titleTextView);
+        TextView contentTextView = findViewById(R.id.contentTextView);
+
+        itemTextView.setText(shortTitles[itemIndex]);
+        titleTextView.setText(titles[itemIndex].trim());
+        contentTextView.setText(contents[itemIndex].trim());
+
+        String imageName = imageNames[itemIndex].trim();
+        int imageResId = getResources().getIdentifier(imageName, "drawable", getPackageName());
+        detailImageView.setImageResource(imageResId);
     }
 }

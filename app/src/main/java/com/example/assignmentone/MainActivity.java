3fedd42598e6d203ba158d72d05a34dba58105e8
wbhaoshuai <1402078577@qq.com>
2025-10-25 16:45:51 +0800

    Add click event for imagebutton.

--- app/src/main/java/com/example/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.assignmentone;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onClickFacilities(View view){
+        Toast.makeText(this, "Facilities clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickEvents(View view){
+        Toast.makeText(this, "Events clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickClubs(View view){
+        Toast.makeText(this, "Clubs clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void  onClickSupport(View view){
+        Toast.makeText(this, "Support clicked", Toast.LENGTH_SHORT).show();
     }
 }

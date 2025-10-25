baf1a7dadf8f1c4ee29916f151203a9c132675b9
wbhaoshuai <1402078577@qq.com>
2025-10-25 19:31:13 +0800

    Change the sent content to an integer to facilitate the positioning of array content.

--- app/src/main/java/com/example/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -14,11 +14,10 @@ import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     // Define a unique identifier constant
-    public static final String EXTRA_ITEM_TYPE = "com.example.assignmentone.ITEM_TYPE";
-    public static final String TYPE_FACILITIES = "facilities";
-    public static final String TYPE_EVENTS = "events";
-    public static final String TYPE_CLUBS = "clubs";
-    public static final String TYPE_SUPPORT = "support";
+    public static final int POSITION_FACILITIES = 0;
+    public static final int POSITION_EVENTS = 1;
+    public static final int POSITION_CLUBS = 2;
+    public static final int POSITION_SUPPORT = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,28 +34,28 @@ public class MainActivity extends AppCompatActivity {
     public void onClickFacilities(View view){
         Toast.makeText(this, "Facilities clicked", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(EXTRA_ITEM_TYPE, TYPE_FACILITIES);
+        intent.putExtra("POSITION", POSITION_FACILITIES);
         startActivity(intent);
     }
 
     public void onClickEvents(View view){
         Toast.makeText(this, "Events clicked", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(EXTRA_ITEM_TYPE, TYPE_EVENTS);
+        intent.putExtra("POSITION", POSITION_EVENTS);
         startActivity(intent);
     }
 
     public void onClickClubs(View view){
         Toast.makeText(this, "Clubs clicked", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(EXTRA_ITEM_TYPE, TYPE_CLUBS);
+        intent.putExtra("POSITION", POSITION_CLUBS);
         startActivity(intent);
     }
 
     public void  onClickSupport(View view){
         Toast.makeText(this, "Support clicked", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(EXTRA_ITEM_TYPE, TYPE_SUPPORT);
+        intent.putExtra("POSITION", POSITION_SUPPORT);
         startActivity(intent);
     }
 }
